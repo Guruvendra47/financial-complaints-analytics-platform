b804123c524589d53439e4cfdca161218ff2e5d2
Guruvendra47 <Guruvendra47@gmail.com>
2026-03-26 16:36:24 -0400

    Update snowpipe_automation.sql
    Signed-off-by: Guruvendra47 <Guruvendra47@gmail.com>

--- data_pipeline/snowflake_sql/snowpipe_automation.sql
+++ data_pipeline/snowflake_sql/snowpipe_automation.sql
@@ -6,31 +6,31 @@
 --- event notifications sent from an S3 bucket to an SQS queue when new data is ready to load.
 
 -- Table 1
-CREATE PIPE IF NOT EXISTS stock_pipe 
+CREATE PIPE IF NOT EXISTS stock_pipe_customer
 AUTO_INGEST = TRUE 
 AS
-COPY INTO raw_stock_data
-FROM @realtime_stage/table_name(mention your table name)
+COPY INTO customer_table -- mention table name in which you want to copy the data
+FROM @realtime_stage/customer --stage/s3 bucket folder name
 FILE_FORMAT = complaints_parquet_format
 PATTERN = '.*\\.parquet'
 MATCH_BY_COLUMN_NAME = CASE_INSENSITIVE;
 
 -- Table 2
-CREATE PIPE IF NOT EXISTS stock_pipe 
+CREATE PIPE IF NOT EXISTS stock_pipe_channel
 AUTO_INGEST = TRUE 
 AS
-COPY INTO raw_stock_data
-FROM @realtime_stage/channel_table
+COPY INTO channel_table
+FROM @realtime_stage/channel
 FILE_FORMAT = complaints_parquet_format
 PATTERN = '.*\\.parquet'
 MATCH_BY_COLUMN_NAME = CASE_INSENSITIVE;
 
 -- Table 3
-CREATE PIPE IF NOT EXISTS stock_pipe 
+CREATE PIPE IF NOT EXISTS stock_pipe_location
 AUTO_INGEST = TRUE 
 AS
-COPY INTO raw_stock_data
-FROM @realtime_stage/location_table
+COPY INTO location_table
+FROM @realtime_stage/location
 FILE_FORMAT = complaints_parquet_format
 PATTERN = '.*\\.parquet'
 MATCH_BY_COLUMN_NAME = CASE_INSENSITIVE;
@@ -50,41 +50,24 @@ SHOW PIPES;
 ----------------------------------------------------------PIPEREFRESH-----------------------------------------------------------------
 
 -- Client loaded data in AWS you need to execute following command to referesh the folder that data will be loaded automatically
-ALTER PIPE RETAIL_SNOWPIPE_DEMOGRAPHIC refresh;
-ALTER PIPE  RETAIL_SNOWPIPE_CAMPAIGN_DESC refresh;
-ALTER PIPE  RETAIL_SNOWPIPE_CAMPAIGN refresh;
-ALTER PIPE  RETAIL_SNOWPIPE_PRODUCT refresh;
-ALTER PIPE  RETAIL_SNOWPIPE_COUPON refresh;
-ALTER PIPE  RETAIL_SNOWPIPE_COUPON_REDEMPT refresh;
-ALTER PIPE  RETAIL_SNOWPIPE_TRANSACTION refresh;
+ALTER PIPE stock_pipe_customer refresh;
+ALTER PIPE  stock_pipe_channel refresh;
+ALTER PIPE  stock_pipe_location refresh;
 
 ---------------------------------------------------------Count-Star(*)------------------------------------------------------------------
 
 -- Execute the following command when you want to check number of transaction
 -- when you want to check table number one by one.
-SELECT COUNT(*) FROM demographic_RAW;
-SELECT COUNT(*) FROM CAMPAIGN_DESC_RAW;
-SELECT COUNT(*) FROM CAMPAIGN_RAW;
-SELECT COUNT(*) FROM PRODUCT_RAW;
-SELECT COUNT(*) FROM COUPON_RAW;
-SELECT COUNT(*) FROM COUPON_REDEMPT_RAW;
-SELECT COUNT(*) FROM TRANSACTION_RAW;
-
+SELECT COUNT(*) FROM cusotmer_table;
+SELECT COUNT(*) FROM channel_table;
+SELECT COUNT(*) FROM location_table;
 
 -- When you want to count all the table together 
-SELECT COUNT(*) FROM demographic_RAW
+SELECT COUNT(*) FROM customer_table
 UNION ALL
-SELECT COUNT(*) FROM CAMPAIGN_DESC_RAW
+SELECT COUNT(*) FROM channel_table
 UNION ALL
-SELECT COUNT(*) FROM CAMPAIGN_RAW
-UNION ALL
-SELECT COUNT(*) FROM PRODUCT_RAW
-UNION ALL
-SELECT COUNT(*) FROM COUPON_RAW
-UNION ALL
-SELECT COUNT(*) FROM COUPON_REDEMPT_RAW
-UNION ALL
-SELECT COUNT(*) FROM TRANSACTION_RAW;
+SELECT COUNT(*) FROM location_table
 
 --------------------------------------------------------Snowpipe-Stauts-----------------------------------------------------------------
 
@@ -92,19 +75,20 @@ SELECT COUNT(*) FROM TRANSACTION_RAW;
 -- Following are some snowpipe command which will help you to check snowpipe status 
 -- count command count the no of rows but this command very very important which help to verify the count.
 
-select SYSTEM$PIPE_STATUS('RETAIL_SNOWPIPE_TRANSACTION'); -- Enter the snowpipe full name  
+select SYSTEM$PIPE_STATUS('ENTER YOUR SNOWPIPE NAME'); -- Enter the snowpipe full name  
 
-select * from table(information_schema.copy_history(table_name=>'TRANSACTION_RAW', start_time=>
-dateadd(hours, -1, current_timestamp())));
+select * 
+from table 
+  (information_schema.copy_history(table_name=>'TRANSACTION_RAW', 
+  start_time=> dateadd(hours, 
+  -1, 
+  current_timestamp())));
 -- you enter snowfake folder name you created
 
 -----------------------------------------------------To-Check-The-All-Data--------------------------------------------------------------
 
 -- To check all table data we execute this code
-SELECT * FROM demographic_RAW;
-SELECT * FROM CAMPAIGN_DESC_RAW;
-SELECT * FROM CAMPAIGN_RAW;
-SELECT * FROM PRODUCT_RAW;
-SELECT * FROM COUPON_RAW;
-SELECT * FROM COUPON_REDEMPT_RAW;
-SELECT * FROM TRANSACTION_RAW;
+SELECT * FROM customer_table;
+SELECT * FROM channel_table;
+SELECT * FROM location_table;
+
